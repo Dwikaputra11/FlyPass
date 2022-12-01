a18c25db27a1ac6e149416ceb214cd1d3bc6f5a4
zaabsyid <abdulrosyid2002@gmail.com>
2022-12-01 22:22:39 +0700

    add setup api flight & airport

--- user/app/src/main/java/cthree/user/flypass/viewmodels/AirportViewModel.kt
+++ user/app/src/main/java/cthree/user/flypass/viewmodels/AirportViewModel.kt
@@ -1,4 +1,44 @@
 package cthree.user.flypass.viewmodels
 
-class AirportViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import cthree.user.flypass.api.APIClient
+import cthree.user.flypass.models.airport.Airport
+import cthree.user.flypass.models.flight.Flight
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class AirportViewModel : ViewModel() {
+
+    lateinit var liveDataAirport : MutableLiveData<List<Airport>?>
+
+    init {
+        liveDataAirport = MutableLiveData()
+    }
+
+    fun getLiveDataAirports() : MutableLiveData<List<Airport>?>{
+        return liveDataAirport
+    }
+
+    fun callApiAirport(){
+        APIClient.instance.apiServiceAirport()
+            .enqueue(object : Callback<List<Airport>> {
+                override fun onResponse(
+                    call: Call<List<Airport>>,
+                    response: Response<List<Airport>>
+                ) {
+                    if (response.isSuccessful){
+                        liveDataAirport.postValue(response.body())
+                    } else{
+                        liveDataAirport.postValue(null)
+                    }
+                }
+
+                override fun onFailure(call: Call<List<Airport>>, t: Throwable) {
+                    liveDataAirport.postValue(null)
+                }
+            })
+    }
+
 }

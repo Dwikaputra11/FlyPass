bd0ce66fb04ce22c7f0c7ff19ff46ff0a6d08ae0
Dwika <dwikariswanda@gmail.com>
2022-12-01 19:07:55 +0700

    Slicing Profile by Syifa

---
+++ user/app/src/main/java/cthree/user/flypass/ui/profile/ProfileAccountInfoFragment.kt
@@ -0,0 +1,24 @@
+package cthree.user.flypass.ui.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import cthree.user.flypass.R
+
+class ProfileAccountInfoFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile_account_info, container, false)
+    }
+
+}

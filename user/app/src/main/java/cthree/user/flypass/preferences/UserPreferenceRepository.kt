3ccd734fc57d952648df6c3eb69a86367a527638
zaabsyid <abdulrosyid2002@gmail.com>
2022-12-02 19:43:03 +0700

    add Proto Depart Arrive Airport

--- user/app/src/main/java/cthree/user/flypass/preferences/UserPreferenceRepository.kt
+++ user/app/src/main/java/cthree/user/flypass/preferences/UserPreferenceRepository.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.datastore.core.DataStore
 import androidx.datastore.dataStore
 import cthree.user.flypass.UserProto
+import cthree.user.flypass.models.airport.Airport
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import java.io.IOException
@@ -23,8 +24,40 @@ class UserPreferenceRepository(private val context: Context) {
     }
 
     // save data to data store proto
-    suspend fun saveData() {
+    suspend fun saveDataAirportArrive(airport: Airport) {
+        //Depart
+        context.userPreferencesStore.updateData { preferences ->
+            preferences.toBuilder().setDepartAirportCity(airport.city).build()
+        }
+        context.userPreferencesStore.updateData { preferences ->
+            preferences.toBuilder().setDepartAirportCountry(airport.country).build()
+        }
+        context.userPreferencesStore.updateData { preferences ->
+            preferences.toBuilder().setDepartAirportIata(airport.iata).build()
+        }
+        context.userPreferencesStore.updateData { preferences ->
+            preferences.toBuilder().setDepartAirportId(airport.id.toInt()).build()
+        }
+        context.userPreferencesStore.updateData { preferences ->
+            preferences.toBuilder().setDepartAirportName(airport.name).build()
+        }
 
+        //Arrive
+        context.userPreferencesStore.updateData { preferences ->
+            preferences.toBuilder().setArriveAirportCity(airport.city).build()
+        }
+        context.userPreferencesStore.updateData { preferences ->
+            preferences.toBuilder().setArriveAirportCountry(airport.country).build()
+        }
+        context.userPreferencesStore.updateData { preferences ->
+            preferences.toBuilder().setArriveAirportIata(airport.iata).build()
+        }
+        context.userPreferencesStore.updateData { preferences ->
+            preferences.toBuilder().setArriveAirportId(airport.id.toInt()).build()
+        }
+        context.userPreferencesStore.updateData { preferences ->
+            preferences.toBuilder().setArriveAirportName(airport.name).build()
+        }
     }
 
 
@@ -32,6 +65,44 @@ class UserPreferenceRepository(private val context: Context) {
     suspend fun clearData(){
 
     }
+
+    suspend fun clearDataDepartArrival(){
+        //Depart
+        context.userPreferencesStore.updateData { preferences ->
+            preferences.toBuilder().clearDepartAirportCity().build()
+        }
+        context.userPreferencesStore.updateData { preferences ->
+            preferences.toBuilder().clearDepartAirportCountry().build()
+        }
+        context.userPreferencesStore.updateData { preferences ->
+            preferences.toBuilder().clearDepartAirportIata().build()
+        }
+        context.userPreferencesStore.updateData { preferences ->
+            preferences.toBuilder().clearDepartAirportId().build()
+        }
+        context.userPreferencesStore.updateData { preferences ->
+            preferences.toBuilder().clearDepartAirportName().build()
+        }
+
+        //Arrive
+        context.userPreferencesStore.updateData { preferences ->
+            preferences.toBuilder().clearArriveAirportCity().build()
+        }
+        context.userPreferencesStore.updateData { preferences ->
+            preferences.toBuilder().clearArriveAirportCountry().build()
+        }
+        context.userPreferencesStore.updateData { preferences ->
+            preferences.toBuilder().clearArriveAirportIata().build()
+        }
+        context.userPreferencesStore.updateData { preferences ->
+            preferences.toBuilder().clearArriveAirportId().build()
+        }
+        context.userPreferencesStore.updateData { preferences ->
+            preferences.toBuilder().clearArriveAirportName().build()
+        }
+
+    }
+
 
     // read data store proto
     private val TAG: String = "UserPreferencesRepo"
